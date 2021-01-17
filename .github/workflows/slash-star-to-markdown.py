ad26e557f87e8e4e1c0486c37b117f274bde07c5
odaniel1 <owendnl@gmail.com>
2021-01-17 16:19:44 +0000

    defined parse_ss_to_md as function

--- .github/workflows/slash-star-to-markdown.py
+++ .github/workflows/slash-star-to-markdown.py
@@ -1,47 +1,56 @@
+
 import re
 
-# matches start /* of slash-star comment block
-ss_start = re.compile('\\/\\*')
+# %% parses a string removing /* and */ from the start and end of slash-star
+# comment blocks, and placing code outside of blocks into markdown code chunks.
 
-# matches end */ of slash-star comment block
-ss_end = re.compile('\\*\\/')
+def parse_ss_to_md(fp):
 
-# path to the query to transcripe
-ss_fp = 'example.sql'
-
-with open(ss_fp, 'r') as ss:
-    # read lines into memory
-    ss_string = ss.read()
+    # regular expressions to match (respectively) /* and */
+    ss_start = re.compile('\\/\\*')
+    ss_end = re.compile('\\*\\/')
     
-    # replace /* and */ with markdown start/end blocks
-    md_string = ss_start.sub('```\n', ss_string)
-    md_string = ss_end.sub('\n```sql', md_string)
+    # read the file into memory, and substitute slash-star comments for
+    # markdown comment blocks; note the start of a slash-star comment is 
+    # preceded by the end of a code chunk, and the end of a slash-star comment
+    # is succeeded by the start of a code chunk.
+    with open(fp, 'r') as ss:
+        ss_str = ss.read()
+        md_str = ss_start.sub('```\n', ss_str)
+        md_str = ss_end.sub('\n```sql', md_str)
+    
+    # regular expressions to match respectively leading ``` at the start of a
+    # string, and trailing ``` at the end of a string.
+    md_lead = re.compile('^[\n\s]{0,}```')
+    md_trail = re.compile('```sql[\n\s]{0,}\Z')
+    
+    # if the file started with a slash-star comment block - we need to remove
+    # the leading ``` (there's no code chunk above to close).
+    # Conversely if there is no leading ``` then there should be (as this 
+    # indicates the file started with code).
+    if md_lead.match(md_str):
+        md_str = md_lead.sub('', md_str)
+    else:
+        md_str = '```sql\n' + md_str
+    
+    # similar logic applies to trailing ```: we need to remove it if it exists,
+    # or add it if it didn't.
+    if md_trail.search(md_str):
+        # if so - need to remove the trailing ```
+        md_str = md_trail.sub('', md_str)
+    else:
+        # if not - need to add a trailing ```
+        md_str = md_str + '```'
+        
+    return md_str
 
+# %% Run script
 
-# matches leading ``` at the start of a file
-md_lead = re.compile('^[\n\s]{0,}```')
+# parsed markdown string, and file to write out to 
+md_str = parse_ss_to_md('example.sql')
 
-# matches trailing ``` at the end of a file
-md_trail = re.compile('```sql[\n\s]{0,}\Z')
-
-# did the file start with a comment or not?
-if md_lead.match(md_string):
-    # if so - need to remove the leading ```
-    md_string = md_lead.sub('', md_string)
-else:
-    # if not - need to add a leading ```
-    md_string = '```sql\n' + md_string
-
-# did the file end with a comment or not?
-if md_trail.search(md_string):
-    # if so - need to remove the trailing ```
-    md_string = md_trail.sub('', md_string)
-else:
-    # if not - need to add a trailing ```
-    md_string = md_string + '```'
-
-# write transcribed file to README
 md_fp = 'README.md'
 
+# write file out
 with open(md_fp, 'w') as md:
-    md.write(md_string)
+    md.write(md_str)
